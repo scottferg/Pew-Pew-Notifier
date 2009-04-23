b989edfb68a37c47c5fa75e1763973d280560413
Bill Best <wmbest2@gmail.com>
2009-04-23 02:38:49 +0000

    Initial Commit of Twitter Plugin

--- ProtocolChecker/src/plugins/TwitterCheck.py
+++ ProtocolChecker/src/plugins/TwitterCheck.py
@@ -6,16 +6,27 @@ Created on Apr 15, 2009
 
 import twitter
 import Plugin
+import TwitterCheckUI
 
 class TwitterCheck( Plugin.Plugin ):
-    def twitterFeed( uid, password ):
-        api = twitter.Api( username = uid, password = password )
-        friend_timeline = api.GetFriendsTimeline( uid )
+    def check(self):
+        if self.counter == self.ui.get_update():
+            self.counter = 0
+            self.update()
+        else:
+            self.counter += 15000
+    def update( self ):
+        
+        api = twitter.Api( username = ui.get_username, password = ui.get_password )
+        friend_timeline = api.GetFriendsTimeline( username, None, None, None, None )
     
-        return friend_timeline
+        return trigger_alert( friend_timeline )
 
     def show( self ):
+        self.ui.show( )
         return
     
     def __init__( self ):
+        self.ui = TwitterCheckUI.TwitterCheckUI( )
+        self.counter = 0
         return
