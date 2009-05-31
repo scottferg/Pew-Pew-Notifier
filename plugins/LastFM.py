5db679ca2a78e0fa9e6d3a464bd4f02f31aec42e
Scott Ferguson <scottwferg@gmail.com>
2009-05-31 12:46:57 -0500

    Initial development for Last.fm plugin

---
+++ plugins/LastFM.py
@@ -0,0 +1,14 @@
+'''
+Created on May 31, 2009
+
+@author: Scott Ferguson
+'''
+
+import Plugin
+
+class LastFM( Plugin.Plugin ):
+    API_KEY = '6bd4fd1a3a707c7634941a2920d65dcf'
+    SECRET_KEY = 'a20918ddb42bbec4fe005e7f52e501be'
+
+    def __init( self ):
+        pass
